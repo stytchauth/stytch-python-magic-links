820892daa727cd5e4762a2359ee502f1d9668a0b
Grace Baelen-King <70169804+grace-stytch@users.noreply.github.com>
2021-02-23 09:39:13 -0800

    Add Initial Project (#1)
    * initial project
    
    * feedback

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from stytch import Client
+
+# load the .env file
+load_dotenv()
+
+# pull in configuration from .env file
+PORT = os.getenv("PORT")
+HOST = os.getenv("HOST")
+
+magic_link_url = "http://{0}:{1}/authenticate".format(host, port)
+
+# define the stytch client using your stytch project id & secret
+# set environment to "live" if you want to hit the live api
+stytch_client = Client(
+    project_id=os.getenv("STYTCH_PROJECT_ID"),
+    secret=os.getenv("STYTCH_SECRET"),
+    environment="test",
+)
+
+# create a Flask web app
+app = Flask(__name__)
+
+
+# handles the homepage for Hello Socks
+@app.route('/')
+def index():
+    return render_template('loginOrSignUp.html')
+
+
+# takes the email entered on the homepage and hits the stytch
+# loginOrCreateUser endpoint to send the user a magic link
+@app.route('/login_or_create_user', methods=['POST'])
+def login_or_create_user():
+    r = stytch_client.Users.delete("userid")
+    resp = stytch_client.Users.update(
+        user_id="userid",
+        first_name="Jane",
+        last_name="Doe",
+        email="email"
+    )
+    print(resp)
+    resp = stytch_client.MagicLinks.login_or_create(
+        email=request.form['email'],
+        login_magic_link_url=magic_link_url,
+        signup_magic_link_url=magic_link_url
+    )
+
+    if resp.status_code != 200:
+        print(resp)
+        return "something went wrong sending magic link"
+    return render_template('emailSent.html')
+
+
+# This is the endpoint the link in the magic link hits. It takes the token from the
+# link's query params and hits the stytch authenticate endpoint to verify the token is valid
+@app.route('/authenticate')
+def authenticate():
+    stytch_client.MagicLinks.revoke_invite_by_email(email="")
+    resp = stytch_client.MagicLinks.authenticate(request.args.get('token'))
+
+    if resp.status_code != 200:
+        print(resp)
+        return "something went wrong authenticating token"
+    return render_template('loggedIn.html')
+
+
+# handles the logout endpoint
+@app.route('/logout')
+def logout():
+    return render_template('loggedOut.html')
+
+
+# run's the app on the provided host & port
+if __name__ == '__main__':
+    # in production you would want to make sure to disable debugging
+    app.run(host=host, port=port, debug=True)
