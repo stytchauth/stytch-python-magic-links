02ff9b7a0005198b863c280f0cea1710b723fe53
Logan Gore <lgore@stytch.com>
2022-12-18 20:40:12 -0500

    Update for stytch v6.0.0

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-import os
+#!/usr/bin/env python3
 
+import os
+import sys
+
+import dotenv
+import stytch
 from flask import Flask, render_template, request
-from dotenv import load_dotenv
-from stytch import Client
 
 # load the .env file
-load_dotenv()
+dotenv.load_dotenv()
 
-# pull in configuration from .env file
-PORT = os.getenv("PORT")
-HOST = os.getenv("HOST")
+# By default, run on localhost:8080
+PORT = os.getenv("PORT", "localhost")
+HOST = os.getenv("HOST", "8080")
+MAGIC_LINK_URL = f"http://{HOST}:{PORT}/authenticate"
 
-magic_link_url = "http://{0}:{1}/authenticate".format(HOST, PORT)
+# Load the Stytch credentials, but quit if they aren't defined
+STYTCH_PROJECT_ID = os.getenv("STYTCH_PROJECT_ID")
+STYTCH_SECRET = os.getenv("STYTCH_SECRET")
+if STYTCH_PROJECT_ID is None:
+    sys.exit("STYTCH_PROJECT_ID env variable must be set before running")
+if STYTCH_SECRET is None:
+    sys.exit("STYTCH_SECRET env variable must be set before running")
 
-# define the stytch client using your stytch project id & secret
-# set environment to "live" if you want to hit the live api
-stytch_client = Client(
-    project_id=os.getenv("STYTCH_PROJECT_ID"),
-    secret=os.getenv("STYTCH_SECRET"),
+# NOTE: Set environment to "live" if you want to hit the live api
+stytch_client = stytch.Client(
+    project_id=STYTCH_PROJECT_ID,
+    secret=STYTCH_SECRET,
     environment="test",
 )
 
@@ -26,46 +35,47 @@ app = Flask(__name__)
 
 
 # handles the homepage for Hello Socks
-@app.route('/')
-def index():
-    return render_template('loginOrSignUp.html')
+@app.route("/")
+async def index() -> str:
+    return render_template("loginOrSignUp.html")
 
 
 # takes the email entered on the homepage and hits the stytch
 # loginOrCreateUser endpoint to send the user a magic link
-@app.route('/login_or_create_user', methods=['POST'])
-def login_or_create_user():
-    resp = stytch_client.magic_links.email.login_or_create(
-        email=request.form['email'],
-        login_magic_link_url=magic_link_url,
-        signup_magic_link_url=magic_link_url
+@app.route("/login_or_create_user", methods=["POST"])
+async def login_or_create_user() -> str:
+    resp = await stytch_client.magic_links.email.login_or_create(
+        email=request.form["email"],
+        login_magic_link_url=MAGIC_LINK_URL,
+        signup_magic_link_url=MAGIC_LINK_URL,
     )
 
     if resp.status_code != 200:
         print(resp)
         return "something went wrong sending magic link"
-    return render_template('emailSent.html')
+    return render_template("emailSent.html")
 
 
-# This is the endpoint the link in the magic link hits. It takes the token from the
-# link's query params and hits the stytch authenticate endpoint to verify the token is valid
-@app.route('/authenticate')
-def authenticate():
-    resp = stytch_client.magic_links.authenticate(request.args.get('token'))
+# This is the endpoint the link in the magic link hits.
+# It takes the token from the link's query params and hits the
+# stytch authenticate endpoint to verify the token is valid
+@app.route("/authenticate")
+async def authenticate() -> str:
+    resp = await stytch_client.magic_links.authenticate(request.args.get("token"))
 
     if resp.status_code != 200:
         print(resp)
         return "something went wrong authenticating token"
-    return render_template('loggedIn.html')
+    return render_template("loggedIn.html")
 
 
 # handles the logout endpoint
-@app.route('/logout')
-def logout():
-    return render_template('loggedOut.html')
+@app.route("/logout")
+async def logout() -> str:
+    return render_template("loggedOut.html")
 
 
 # run's the app on the provided host & port
-if __name__ == '__main__':
+if __name__ == "__main__":
     # in production you would want to make sure to disable debugging
     app.run(host=HOST, port=PORT, debug=True)
